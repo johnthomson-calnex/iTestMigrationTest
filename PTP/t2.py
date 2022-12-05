616457a4db60aab31447ae3e5c5dea7643a9bad7
John Thomson <john.thomson@calnexsol.com>
2022-12-05 13:18:47 +0000

    test2

---
+++ PTP/t2.py
@@ -0,0 +1,20 @@
+import requests,json,os,sys
+
+def argsToJSON(arg):
+    i = iter(arg)
+    d = dict(zip(i, i))
+    return json.dumps(d)
+
+def p100set(api_path,ip, *arg):
+    return requests.put(f"http://{ip}{api_path}",
+        argsToJSON(arg), headers={'Content-Type': 'application/json'})
+
+
+try:
+    ip = os.environ["ip"]
+    #ip = '100g-vm1'
+    #ip = sys.argv[1]
+    p100set("/api/app/mse/ptpprofile?PtpProfile=Profile_CMCC_5G", ip)
+    
+except Exception as e:
+    print(e)
