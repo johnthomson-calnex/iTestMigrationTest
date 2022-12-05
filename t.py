4ecd2354bd1a740bb129f3a829c027e49ee0c488
John Thomson <john.thomson@calnexsol.com>
2022-12-05 13:14:56 +0000

    take in ip from cmd line

--- t.py
+++ t.py
@@ -1,16 +1,20 @@
-import requests,json,os
+import requests,json,os,sys
 
 def argsToJSON(arg):
     i = iter(arg)
     d = dict(zip(i, i))
     return json.dumps(d)
 
-def p100set(api_path, *arg):
-    return requests.put(f"http://100g-vm4{api_path}",
+def p100set(api_path,ip, *arg):
+    return requests.put(f"http://{ip}{api_path}",
         argsToJSON(arg), headers={'Content-Type': 'application/json'})
 
 
 try:
-    print(os.environ["itest_var"])
+    #ip = os.environ["unit_ip"]
+    #ip = '100g-vm1'
+    ip = sys.argv[1]
+    p100set("/api/app/mse/ptpprofile?PtpProfile=Profile_CCSA", ip)
+    
 except Exception as e:
     print(e)
